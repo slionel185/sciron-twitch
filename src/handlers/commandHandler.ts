3e0153ab5f8c990858cf94f11da334ff46969304
Seth <slionel1850@gmail.com>
2023-06-28 04:33:24 +0000

    Test

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -6,17 +6,17 @@ import utility from '@/functions/utility'
 import getCommandByChannel from '@/functions/getCommandsByChannel'
 
 export default async function commandHandler(channel: string , userState: ChatUserstate, message: string) {
-    const channelName = message.split('#')[1]
+    const channelName = channel.split('#')[0]
     console.log(channelName)
     const commandName = message.split(' ')[0].split('!')[1]
     const commandSender = userState.username?.toLowerCase()
 
-    const channelCommands = await getCommandByChannel(channelName)
+    /*const channelCommands = await getCommandByChannel(channelName)
 
     channelCommands.map(async command => {
         if(channelName.toLowerCase() === commandSender && !command.active) return await bot.say(channel, 'Command not active!')
         
         if(command.active && command.name.toLowerCase() === commandName.toLowerCase() && command.commandType === 'REPLY') return await reply(channel, command)
         if(command.name.toLowerCase() === commandName.toLowerCase() && command.commandType === 'UTILITY') return await utility(channel, command)
-    })
+    })*/
 }
